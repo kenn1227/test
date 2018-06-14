25cbf621a61ec2da141930cb5f8f294a7bf7ed92
noralee <674013578@qq.com>
2018-06-14 09:47:14 +0800

    initial commit

--- src/main/java/ch/example/test/TestApplication.java
+++ src/main/java/ch/example/test/TestApplication.java
@@ -1,5 +1,8 @@
 package ch.example.test;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import ch.example.test.util.SignalHandlerExample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +10,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
 
 	public static void main(String[] args) {
+		Log log=LogFactory.getLog(SignalHandlerExample.class);
 		SpringApplication.run(TestApplication.class, args);
+
+		SignalHandlerExample mySignalHandler = new SignalHandlerExample();
+		while ( mySignalHandler.getRunningStatus() ) {
+			log.info("waiting");
+			mySignalHandler.myWait();
+			log.info("woken up");
+		}
 	}
 }
--- src/main/java/ch/example/test/util/SignalHandlerExample.java
+++ src/main/java/ch/example/test/util/SignalHandlerExample.java
@@ -1,5 +1,81 @@
 package ch.example.test.util;
 
-public class SignalHandlerExample {
-    
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sun.misc.Signal;
+import sun.misc.SignalHandler;
+
+public class SignalHandlerExample implements SignalHandler{
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+
+    private boolean Running = true;
+
+    public SignalHandlerExample() {
+        this.setupHandler();
+    }
+
+    private void setupHandler() {
+        this.handleSignal("INT");
+        this.handleSignal("TERM");
+        this.handleSignal("HUP");
+    }
+
+    public void handleSignal(final String signalName) {
+        try {
+            Signal.handle(new Signal(signalName), this);
+        } catch (IllegalArgumentException e) {
+            // 可能这个信号,并不支持这个平台或JVM作为目前配置
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 捕获信号
+     * @param signal The {@link sun.misc.Signal} that we received
+     **/
+    public synchronized void handle(Signal signal) {
+        // we are only interested in catching the "TERM" signal
+        if ( signal.getName().equals("TERM") ) {
+            log.info(" TERM received " + signal.getName() );
+            log.info("threadName" + Thread.currentThread().getName());
+            System.exit(-1);
+        } else if ( signal.getName().equals("INT") ||signal.getName().equals("HUP") ) {
+            setRunningStatus( false );
+            notifyAll(); //TODO - clarify if notify OR notifyall
+        }
+        log.info( signal.getName() + " " + signal.getNumber() );
+        log.info( getRunningStatus()+"" );
+    }
+
+    public void setRunningStatus( boolean Status ) {
+        this.Running = Status;
+    }
+
+    public boolean getRunningStatus() {
+        return Running;
+    }
+
+    /**
+     * General "clean up" method which is called when we receive a TERM
+     signal
+     * This will likely be superseeded by specific cleanup code
+     *
+     **/
+    public boolean cleanUp() {
+        // TODO - we need to decide what we are going to do one we trap a
+        //  TERMinate signal.
+        // We need to complete all our threads nicely.
+        log.info("Cleaning up!");
+        return true;
+    }
+
+    public synchronized void myWait() {
+        try{
+            wait();
+        }catch (InterruptedException e){
+            log.info( "myWait WOKEN UP" + e );
+        }
+    }
+
 }
